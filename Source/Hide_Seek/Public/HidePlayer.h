69e8fa3090b6613d080ad676688f03e43840cacf
realhwi <wlsgnl49@gmail.com>
2024-02-28 08:20:40 +0900

    Vr Test

---
+++ Source/Hide_Seek/Public/HidePlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRCharacter.h"
+#include "HidePlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HIDE_SEEK_API AHidePlayer : public AVRCharacter
+{
+	GENERATED_BODY()
+	
+};
