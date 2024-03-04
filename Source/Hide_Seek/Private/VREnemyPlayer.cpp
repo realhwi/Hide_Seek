8326b09865a92bd732f2486b4176ceb0e5c864d7
realhwi <wlsgnl49@gmail.com>
2024-03-04 18:41:51 +0900

    최종 합침!

---
+++ Source/Hide_Seek/Private/VREnemyPlayer.cpp
@@ -0,0 +1,136 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VREnemyPlayer.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "MotionControllerComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+// Sets default values
+AVREnemyPlayer::AVREnemyPlayer()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	cameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	cameraComp->SetupAttachment(GetRootComponent());
+	cameraComp->bUsePawnControlRotation = true;
+	cameraComp->SetRelativeLocation(FVector(0, 0, 60));
+
+	leftMotionCtrl = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("LeftMotionCtrl"));
+	leftMotionCtrl->SetupAttachment(GetRootComponent());
+	leftMotionCtrl->SetTrackingMotionSource(FName("Left"));
+
+	rightMotionCtrl = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("RightMotionCtrl"));
+	rightMotionCtrl->SetupAttachment(GetRootComponent());
+	rightMotionCtrl->SetTrackingMotionSource(FName("Right"));
+
+	leftHandMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("LeftHandMesh"));
+	leftHandMesh->SetupAttachment(leftMotionCtrl);
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempLeftHandMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/MannequinsXR/Meshes/SKM_QuinnXR_left.SKM_QuinnXR_left'"));
+
+	if (tempLeftHandMesh.Succeeded())
+	{
+		leftHandMesh->SetSkeletalMesh(tempLeftHandMesh.Object);
+		leftHandMesh->SetRelativeLocationAndRotation(FVector(-3, -3.5, 4.5), FRotator(-25, -180, 90));
+	}
+
+	rightHandMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("RightHandMesh"));
+	rightHandMesh->SetupAttachment(rightMotionCtrl);
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempRightHandMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/MannequinsXR/Meshes/SKM_QuinnXR_right.SKM_QuinnXR_right'"));
+
+	if (tempRightHandMesh.Succeeded())
+	{
+		rightHandMesh->SetSkeletalMesh(tempRightHandMesh.Object);
+		rightHandMesh->SetRelativeLocationAndRotation(FVector(-3, 3.5, 4.5), FRotator(25, 0, 90));
+	}
+}
+
+// Called when the game starts or when spawned
+void AVREnemyPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//UHeadMountedDisplayFunctionLibrary::SetTrackingOrigin(EHMDTrackingOrigin::Eye);
+
+	APlayerController* playerController = Cast<APlayerController>(GetWorld()->GetFirstPlayerController());
+	if(playerController)
+	{
+		ULocalPlayer* localPlayer = playerController->GetLocalPlayer();
+		if (localPlayer)
+		{
+			UEnhancedInputLocalPlayerSubsystem* subSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(localPlayer);
+			if(subSystem)
+			{
+				subSystem->AddMappingContext(IMC_VREnemyInput, 0);
+			}
+		}
+	}
+}
+
+// Called every frame
+void AVREnemyPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AVREnemyPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	auto inputSystem = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	if (inputSystem)
+	{
+		inputSystem->BindAction(IA_VREnemyMove, ETriggerEvent::Triggered, this, &AVREnemyPlayer::EnemyMove);
+		inputSystem->BindAction(IA_VREnemyLook, ETriggerEvent::Triggered, this, &AVREnemyPlayer::EnemyLook);
+
+		inputSystem->BindAction(IA_EnemyInteraction, ETriggerEvent::Started, this, &AVREnemyPlayer::EInteractionStart);
+		inputSystem->BindAction(IA_EnemyInteraction, ETriggerEvent::Ongoing, this, &AVREnemyPlayer::EInteractionOnGoing);
+		inputSystem->BindAction(IA_EnemyInteraction, ETriggerEvent::Completed, this, &AVREnemyPlayer::EInteractionComplete);
+		
+	}
+}
+
+void AVREnemyPlayer::EnemyMove(const FInputActionValue& value)
+{
+	FVector2D movementVector = value.Get<FVector2D>();
+
+	AddMovementInput(cameraComp->GetForwardVector(), movementVector.Y);
+	AddMovementInput(cameraComp->GetRightVector(), movementVector.X);
+}
+
+void AVREnemyPlayer::EnemyLook(const FInputActionValue& value)
+{
+	FVector2D lookVector = value.Get<FVector2D>();
+
+	if(nullptr != Controller)
+	{
+		AddControllerYawInput(lookVector.X);
+		AddControllerPitchInput(-lookVector.Y);
+	}
+}
+
+void AVREnemyPlayer::EInteractionStart(const FInputActionValue& value)
+{
+
+}
+
+void AVREnemyPlayer::EInteractionOnGoing(const FInputActionValue& value)
+{
+
+}
+
+void AVREnemyPlayer::EInteractionComplete(const FInputActionValue& value)
+{
+
+}
+
---
+++ Source/Hide_Seek/Public/VREnemyPlayer.h
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "InputMappingContext.h"
+#include "VREnemyPlayer.generated.h"
+
+UCLASS()
+class HIDE_SEEK_API AVREnemyPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVREnemyPlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float moveSpeed = 600;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UInputMappingContext* IMC_VREnemyInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UInputAction* IA_VREnemyMove;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UInputAction* IA_VREnemyLook;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UInputAction* IA_EnemyInteraction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UCameraComponent* cameraComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UMotionControllerComponent* leftMotionCtrl;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UMotionControllerComponent* rightMotionCtrl;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class USkeletalMeshComponent* leftHandMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class USkeletalMeshComponent* rightHandMesh;
+
+private:
+	UFUNCTION()
+	void EnemyMove(const FInputActionValue& value);
+
+	UFUNCTION()
+	void EnemyLook(const FInputActionValue& value);
+
+	UFUNCTION()
+	void EInteractionStart(const FInputActionValue& value);
+
+	UFUNCTION()
+	void EInteractionOnGoing(const FInputActionValue& value);
+
+	UFUNCTION()
+	void EInteractionComplete(const FInputActionValue& value);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float activeSkillTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float activeMaxSkillTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool activeFirstSkill;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool activeSecondSkill;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool activeThirdSkill;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 CountHPChip;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool inAltar;
+};
